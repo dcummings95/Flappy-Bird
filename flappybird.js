46858450753db54e26f240dae166eb32a4574ebc
Dominic Cummings <dominiccummings95@gmail.com>
2023-08-31 19:33:25 -0500

    Initializing starter variables

--- flappybird.js
+++ flappybird.js
@@ -1,8 +1,26 @@
 //board
 let board;
+
+//Pixels set to size of background image
 let boardWidth = 360;
 let boardHeight = 640;
 let context;
+
+//bird
+//birds width/height ratio 408/228 = 17/12 
+let birdWidth = 34; 
+let birdHeight = 24;
+//starting position for bird 
+let birdX = boardWidth/8;
+let birdY = boardHeight/2;
+
+let bird = {
+    x : birdX,
+    y : birdY,
+    width : birdWidth,
+    height : birdHeight
+}
+
 
 window.onload = function() {
     //getting element id from html canvas id
